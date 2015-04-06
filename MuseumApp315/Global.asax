b99a4141e79280f90aa5532ab9d6a624dc28d48c
RobertJMitchell <rjrocks32@gmail.com>
2015-04-06 09:32:13 -0500

    base package

---
+++ MuseumApp315/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MuseumApp315.MvcApplication" Language="C#" %>
---
+++ MuseumApp315/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MuseumApp315.Startup))]
+namespace MuseumApp315
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
